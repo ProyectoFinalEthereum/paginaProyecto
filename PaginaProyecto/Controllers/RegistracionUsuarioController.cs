9a979f64467262ee07331bd715563c779a8b729f
RAMIRO ANDRES COSTA <42100095@ort>
2017-06-23 11:46:54 -0300

    cambios en las views

--- PaginaProyecto/Controllers/RegistracionUsuarioController.cs
+++ PaginaProyecto/Controllers/RegistracionUsuarioController.cs
@@ -9,62 +9,29 @@ namespace PaginaProyecto.Controllers
 {
     public class RegistracionUsuarioController : Controller
     {
-        // GET: RRegistracionUsuario/RegistrarUsuario/
+        // GET: RegistrarUsuario/
         public ActionResult RegistrarUsuario()
         {
             return View();
         }
 
-        // POST : RegistracionUsuario/RegistrarUsuario/
+        // POST : RegistrarUsuario/
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult RegistrarUsuario(Usuario oUsuario)
         {
-            oUsuario.ExisteMail();
-            if (ModelState.IsValid && oUsuario.EmailValido)
+            if (ModelState.IsValid)
             {
-                oUsuario.InsertarUsuario();
-                TempData["UsuarioLogueado"] = oUsuario;
-                return RedirectToAction("HomeUsuario");
+                return RedirectToAction("LoguearUsuario");
             }
             else
             {
-                if (!oUsuario.EmailValido)
-                {
-                    ViewBag.EmailUsado = "El Email ingresado no esta disponible";
-                }
                 return View();
             }
         }
 
-        // GET : RegistracionUsuario/LoguearUsuario
         public ActionResult LoguearUsuario()
         {
-            return View();
-        }
-
-        // POST : RegistracionUsuario/LoguearUsuario
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult LoguearUsuario(Usuario oUsuario)
-        {
-            oUsuario.LoguearUsuario();
-            if (oUsuario.Nombre != null)
-            {
-                TempData["UsuarioLogueado"] = oUsuario;
-                return RedirectToAction("HomeUsuario");
-            }
-            else
-            {
-                ViewBag.MensajeError = "El usuario o la contraseña no son correctos";
-                return View();
-            }
-        }
-
-        // GET : /RegistracionUsuario/HomeUsuario
-        public ActionResult HomeUsuario()
-        {
-            ViewBag.UsuarioLogueado = TempData["UsuarioLogueado"];
             return View();
         }
     }
