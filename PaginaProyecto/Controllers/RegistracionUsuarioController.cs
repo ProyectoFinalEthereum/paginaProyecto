a83d5745733d6295624ffc0e04a1519e600f6074
ramacosta20 <ramiro20andres@gmail.com>
2017-06-25 21:34:24 -0300

    subo cambios

---
+++ PaginaProyecto/Controllers/RegistracionUsuarioController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaginaProyecto.Models;
+
+namespace PaginaProyecto.Controllers
+{
+    public class RegistracionUsuarioController : Controller
+    {
+        // GET: RRegistracionUsuario/RegistrarUsuario/
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        // POST : RegistracionUsuario/RegistrarUsuario/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegistrarUsuario(Usuario oUsuario)
+        {
+            if (ModelState.IsValid)
+            {
+                oUsuario.InsertarUsuario();
+                return RedirectToAction("HomeUsuario");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET : RegistracionUsuario/LoguearUsuario
+        public ActionResult LoguearUsuario()
+        {
+            return View();
+        }
+
+        // POST : RegistracionUsuario/LoguearUsuario
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoguearUsuario(Usuario oUsuario)
+        {
+            Usuario objUsuario = new Usuario();
+            objUsuario = oUsuario.LoguearUsuario(oUsuario);
+            if (objUsuario.Email == oUsuario.Email && oUsuario.Contraseña == objUsuario.Contraseña)
+            {
+                ViewBag.UsuarioLogueado = objUsuario;
+                return RedirectToAction("HomeUsuario");
+            }
+            else
+            {
+                ViewBag.MensajeErrorLogueo = "El usuario o la contraseña no son correctos";
+                return View();
+            }
+        }
+    }
+}
---
+++ PaginaProyecto/Models/Usuario.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaginaProyecto.Models
+{
+    public class Usuario
+    {
+        //declaro conexion
+
+        private MySqlConnection Conexiondb = new MySqlConnection("server=localhost; Uid=root; Password=Proyecto; Database=proyectodb; Port=3306");
+        
+
+        //propiedades de la clase
+        public int UsuarioID { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60, ErrorMessage = "El nombre debe tener menos de 60 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60, ErrorMessage = "El apellido debe tener menos de 60 caracteres")]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Direccion de Email invalida")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Contraseña")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "La contraseña debe tener un minimo de 8 caracteres")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Contraseña", ErrorMessage = "La contraseña y la confirmacion no son iguales")]
+        public string ConfirmarContraseña { get; set; }
+
+        public HttpPostedFileBase Imagen { get; set; }
+
+        //metodos publicos
+
+        //Agrega el usuario que ejecuta el metodo a la base de datos
+        public void InsertarUsuario()
+        {
+            //abro conexion y declaro una transaccion
+            Conexiondb.Open();
+            MySqlTransaction transaccion = Conexiondb.BeginTransaction();
+            try
+            {
+                // asigno el nombre de la consulta a el nombre de consulta que tengo guardado en la DB
+                MySqlCommand Comando = new MySqlCommand("InsertarUsuario", Conexiondb, transaccion);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                //agrego los parametros
+                Comando.Parameters.AddWithValue("PNombre", this.Nombre);
+                Comando.Parameters.AddWithValue("PApellido", this.Apellido);
+                Comando.Parameters.AddWithValue("PEmail", this.Email);
+                Comando.Parameters.AddWithValue("PContrasena", this.Contraseña);
+
+                //ejecuto la consulta que no devuelve nada
+                Comando.ExecuteNonQuery();
+                transaccion.Commit();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    //pruebo la ejecucion de la consulta otra vez
+                    transaccion.Rollback();
+                }
+                catch (Exception ex2)
+                {
+                    //este bloque de codigo va a manejar cualquier error que pudieran 
+                    //ocurrir en el servidor que pudieran causar la falla del reintento,
+                    //como por ejemplo una conexion cerrada.
+                    Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                    Console.WriteLine("  Message: {0}", ex2.Message);
+                }
+            }
+            Conexiondb.Close();
+        }
+
+        //devuelve un Usuario si el mail y la contraseña(parametros) coinciden con un mail y una contraseña de un registro en la DB
+        public Usuario LoguearUsuario(Usuario oUsuario)
+        {
+            Usuario retUsuario = new Usuario();
+            //abro conexion y declaro una transaccion
+            Conexiondb.Open();
+            MySqlTransaction transaccion = Conexiondb.BeginTransaction();
+            try
+            {
+                // asigno el nombre de la consulta a el nombre de consulta que tengo guardado en la DB
+                MySqlCommand Comando = new MySqlCommand("LoguearUsuario", Conexiondb, transaccion);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                //agrego los parametros
+                Comando.Parameters.AddWithValue("PEmail", oUsuario.Email);
+                Comando.Parameters.AddWithValue("PContrasena", oUsuario.Contraseña);
+
+                //ejecuto la consulta y obtengo un iterable con registros
+                MySqlDataReader dr = Comando.ExecuteReader();
+                transaccion.Commit();
+
+                while (dr.Read())
+                {
+                    if (oUsuario.Email == dr["Email"].ToString() && oUsuario.Contraseña == dr["Contrasena"].ToString())
+                    {
+                        retUsuario.Nombre = dr["Nombre"].ToString();
+                        retUsuario.Apellido = dr["Apellido"].ToString();
+                        retUsuario.Email = dr["Email"].ToString();
+                        retUsuario.Contraseña = dr["Contrasena"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    //pruebo la ejecucion de la consulta otra vez
+                    transaccion.Rollback();
+                }
+                catch (Exception ex2)
+                {
+                    //este bloque de codigo va a manejar cualquier error que pudieran 
+                    //ocurrir en el servidor que pudieran causar la falla del reintento,
+                    //como por ejemplo una conexion cerrada.
+                    Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                    Console.WriteLine("  Message: {0}", ex2.Message);
+                }
+            }
+            Conexiondb.Close();
+            return retUsuario;
+        }
+    }
+}
